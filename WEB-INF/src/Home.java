b1de14117f14032fc5e569cf5f950a9e1ccd6f8b
Valentin Thuillier <valentin.thuillier@luxferre-code.fr>
2024-03-08 11:38:07 +0100

    Update index.html with redirect to home page (tomcat)

---
+++ WEB-INF/src/Home.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/home")
+public class Home extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Home Servlet active");
+        RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/view/index.html");
+        rd.forward(req, resp);
+    }
+
+}
